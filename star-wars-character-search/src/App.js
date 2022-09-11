a259f65ae480dd94f61810507f8c12642e3fdf65
Dakota <dakota.x.gilbreath.-nd@disney.com>
2022-09-11 12:42:20 -0700

    added memoized search filter for name and planet

--- star-wars-character-search/src/App.js
+++ star-wars-character-search/src/App.js
@@ -1,18 +1,39 @@
 import { useCharacterData } from './hooks';
 import { CharacterCard } from './CharacterCard';
+import { useState } from 'react';
 
 let id = 100;
 
+const memoSearch = () => {
+  const cache = new Map();
+
+  return (searchTerm, characters = []) => {
+    const query = searchTerm.toLowerCase();
+    if (cache.has(query)) {
+      return cache.get(query);
+    }
+    const filteredList = characters.filter(character => character.homeworld.toLowerCase().search(query) !== -1 || character.name.toLowerCase().search(query) !== -1);
+    cache.set(query, filteredList);
+    return filteredList;
+  }
+}
+
 export default function App() {
-  const { characters, isLoading, isError } = useCharacterData();  
+  const { characters, isLoading, isError } = useCharacterData();
+  const [search, setSearch] = useState('');
+  const filterChars = memoSearch();
+  const filteredList = filterChars(search, characters);
 
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
       {isError && <p>Failed to load character data...</p>}
       {isLoading ? (<p>Loading character data...</p> ) : 
-        characters.map(character => (
-          <CharacterCard key={id++} name={character.name} homeworld={character.homeworld} />
-        ))
+        <div>
+          <input value={search} onChange={e => setSearch(e.target.value)} placeholder='Search by name or home planet...' style={{ width: '25%', margin: '8px' }} />
+          {filteredList.map(character => (
+            <CharacterCard key={id++} name={character.name} homeworld={character.homeworld} />
+          ))}
+        </div>
       }
     </div>
   );
--- star-wars-character-search/src/CharacterCard.js
+++ star-wars-character-search/src/CharacterCard.js
@@ -1,5 +1,5 @@
 export const CharacterCard = ({ name, homeworld }) => (
-    <div style={{ margin: '3px', border: '1px solid gray', borderRadius: '5px', padding: '5px' }}>
+    <div style={{ margin: '8px', border: '1px solid grey', borderRadius: '5px', padding: '8px' }}>
         <h1>{name}</h1>
         <p>{homeworld}</p>
     </div>
